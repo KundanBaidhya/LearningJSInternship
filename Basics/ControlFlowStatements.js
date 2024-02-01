5bb4e8152e2b3e236dcbf5ca568b99eca98ea648
Kundan <baidyapradip005@gmail.com>
2024-02-01 14:26:57 +0545

    looping statements and control flow statements

---
+++ Basics/ControlFlowStatements.js
@@ -0,0 +1,29 @@
+//If-else statement
+
+let truth = 0; 
+if(truth==0){
+    console.log("he's saying the truth");
+}
+else{
+    console.log("he's lying")
+}
+
+//switch statement
+a=4;
+
+switch(a){
+    case 1:
+        console.log("First case is the right one");
+        break;
+    
+    case 2:
+        console.log("Second case is the right one");
+        break;
+    
+    case 3:
+        console.log("Third case is the right one");
+        break;
+    
+    default:
+        console.log("None of the above cases are right");
+}
