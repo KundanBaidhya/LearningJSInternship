8a1ba5b12301885c32bae3fb99abc4863e4ba416
Kundan <baidyapradip005@gmail.com>
2024-02-01 15:36:02 +0545

    Error handling

---
+++ Basics/ErrorHandling.js
@@ -0,0 +1,9 @@
+var months = ["jan", "feb", "mar", "apr", "may", "jun", "july", "aug", "sep", "oct", "nov", "dec"];
+
+try {
+    console.log(months[test]);
+} catch (e) {
+    console.error("An error occured. The error is");
+    console.error(e);
+}
+
