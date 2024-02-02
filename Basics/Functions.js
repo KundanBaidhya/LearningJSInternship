b0cfddf1ff791995f5cc6aac66693847799bf4d2
Kundan <baidyapradip005@gmail.com>
2024-02-02 12:25:52 +0545

    Arrow functions in js

--- Basics/Functions.js
+++ Basics/Functions.js
@@ -1,6 +1,6 @@
-var name = "Kundan";
-var age = 21;
-var hasHobbies = true;
+const name = "Kundan"; //use const as frequently as possible instead of let or var.
+const age = 21;
+const hasHobbies = true;
 
 //creating functions in js
 
@@ -15,3 +15,17 @@ function summarizeUser(userName, userAge, userHasHobbies){ // A function can tak
 console.log(summarizeUser(name, age, hasHobbies)); //calling the function and passing in parameters
 
 //quick note : if it is hardcoded - argument, if it's not - parameter.
+
+//Arrow Functions in js
+
+const a = 12;
+const b = 45;
+
+const add = (x,y) => a+b; //defining an arrow function in js
+const addOne = x => a+1; //if there's only one parameter, () brackets are not needed
+const addRandomNumbers = () => 3+5; //if there's no parameter, just empty line will cause an error
+
+console.log(add(a,b));
+console.log(addOne(a));
+console.log(addRandomNumbers());
+
