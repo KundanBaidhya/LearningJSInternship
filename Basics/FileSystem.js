91ab2923ac3b7d1de976509d8637b2acd4aefcf3
Kundan <baidyapradip005@gmail.com>
2024-02-02 11:18:06 +0545

    file system in node.js

---
+++ Basics/FileSystem.js
@@ -0,0 +1,3 @@
+const fs = require('fs');
+
+fs.writeFileSync('hello.txt','This is what should be inside the hello text file');
--- Basics/LoopingStatements.js
+++ Basics/LoopingStatements.js
@@ -22,3 +22,18 @@ console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~for loop log~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 for(i=0;i<20;i++){
     console.log(i);
 }
+
+
+//for in and for of loop, basically like for each but for keys and values of an array
+const arr = [3,5,7];
+arr.key = "value";
+
+//for in loop
+for(const i in arr){
+    console.log(i);
+}
+
+//for in loop
+for(const i of arr){
+    console.log(i);
+}
