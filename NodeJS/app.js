0a835dec5e12add384b74aeecbc8c51daf2f8d12
Kundan Baidhya <baidyapradip005@gmail.com>
2024-02-06 17:46:09 +0545

    took the incoming stream and then put it in the const message inserted the const message into the message.txt file will insert comment in detail tmr

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,19 +1,36 @@
 const http = require('http');
 const fs = require('fs');
+const { buffer } = require('stream/consumers');
 
 const server = http.createServer((req, res)=>{
-    console.log(req.url, req.method, req.headers);
+    //console.log(req.url, req.method, req.headers);
 
     const url = req.url;
     const method = req.method;
     if(url==="/"){
         res.setHeader('contentType', 'text/html');
+        res.write('<html>');
         res.write('<head><title>My Server</title><head>');
-        res.write("<body><form action='/message' method='POST'><input type='text' message><button type='submit'>weeee</button></form><body>");
+        res.write("<body><form action='/message' method='POST'><input type='text' name='message'><button type='Submit'>weeee</button></form><body>");
+        res.write('</html>');
         return res.end();
     }
     if(url==="/message" && method==="POST"){
-        fs.writeFileSync('message.txt', 'Hello node world');
+        const body = [];
+        req.on('data', (chunk)=>{
+            console.log(chunk);
+            body.push(chunk);
+        });
+
+        req.on('end', ()=>{
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            console.log(message);
+            fs.writeFileSync('message.txt', message);
+        });
+
+        
         res.statusCode = 302;
         res.setHeader('location', '/');
         return res.end();
