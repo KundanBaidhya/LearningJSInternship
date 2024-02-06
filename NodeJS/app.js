beeaabdd1d57902156c4981b683f35f046306544
Kundan Baidhya <baidyapradip005@gmail.com>
2024-02-06 16:50:12 +0545

    made a if else statement to check the url made a form to put in a message with a button that sends us to /message if url is /message and the method is post then creates a file named message.txt and puts in a text no the actual message then redirects to the blank url again using the StatusCode and setHeader function

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,8 +1,28 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res)=>{
     console.log(req.url, req.method, req.headers);
-    process.exit();
+
+    const url = req.url;
+    const method = req.method;
+    if(url==="/"){
+        res.setHeader('contentType', 'text/html');
+        res.write('<head><title>My Server</title><head>');
+        res.write("<body><form action='/message' method='POST'><input type='text' message><button type='submit'>weeee</button></form><body>");
+        return res.end();
+    }
+    if(url==="/message" && method==="POST"){
+        fs.writeFileSync('message.txt', 'Hello node world');
+        res.statusCode = 302;
+        res.setHeader('location', '/');
+        return res.end();
+    }
+    res.setHeader('contentType', 'text/html');
+        res.write('<head><title>My Server</title><head>');
+        res.write('<body><h1>My First Node.js Server</h1><body>');
+        res.end();
+    //process.exit();
 });
 
 server.listen(3000);
