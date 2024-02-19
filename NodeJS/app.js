e656f2a0f91b9dce9946a69ee4ab8f2a74ee8c21
Kundan Baidhya <baidyapradip005@gmail.com>
2024-02-19 16:00:18 +0545

    middleware chain

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -8,8 +8,14 @@ const app = express();
 
 app.use((req, res, next)=> {
     console.log("yay I'm in the middleware now not loading");
-    res.send("hwllo from express");
+    next();
 })
+
+app.use((req, res, next)=> {
+    console.log("yay I'm in the another middleware");
+    res.send("<h1>Hello from express js</h1>");
+})
+
 
 
 const server = http.createServer(app);
