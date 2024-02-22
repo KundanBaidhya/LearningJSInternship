77d60f12b9c383068577beca0da3c5eb6c0c481e
Kundan Baidhya <baidyapradip005@gmail.com>
2024-02-22 15:03:25 +0545

    added the body parser package used the post() method to filter out the http request and filtered the URL using the string parameter in the post method

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next)=> {
-    console.log("yay I'm in the middleware now not loading");
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product',(req, res, next)=> {
+    res.send("<h1> <form action='/myProduct' method='POST'> <input type='text' name='title'> <button type='submit'> Send </button> </form> </h1>");
 })
 
-app.use((req, res, next)=> {
-    console.log("yay I'm in the another middleware");
+app.post('/myProduct',(req, res, next)=> {
+    res.redirect('/');
+    console.log(req.body);
+})
+
+app.use('/',(req, res, next)=> {
     res.send("<h1>Hello from express js</h1>");
 })
 
