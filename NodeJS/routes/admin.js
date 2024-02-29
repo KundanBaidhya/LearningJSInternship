255f129a112eecfcc56216a6dc9c782ac52a8e49
Kundan Baidhya <baidyapradip005@gmail.com>
2024-02-29 15:43:25 +0545

    filters through paths

--- NodeJS/routes/admin.js
+++ NodeJS/routes/admin.js
@@ -2,13 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
+//  /admin/add-product => GET
 router.get('/add-product',(req, res, next)=> {
-    res.send("<form action='/myProduct' method='POST'> <input type='text' name='title'> <button type='submit'> Send </button> </form>");
-})
+    res.send("<form action='/admin/add-product' method='POST'> <input type='text' name='title'> <button type='submit'> Send </button> </form>");
+});
 
-router.post('/myProduct',(req, res, next)=> {
+//  /admin/add-product => POST
+router.post('/add-product',(req, res, next)=> {
     res.redirect('/');
     console.log(req.body);
-})
+});
 
 module.exports = router;
